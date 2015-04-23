d8378363e258a085573e94adc31175f04399228d
Lafwind <lafwind@gmail.com>
2015-04-23 22:28:09 +0800

    function of sqrt

--- sicp.scm
+++ sicp.scm
@@ -25,3 +25,33 @@
        A
        (lambda (i) (+ i 4))
        B))
+
+
+;;; SQRT
+;;; HERON
+
+;;; way 1
+(define (sqrt x)
+  (fixed-point
+   (lambda (y) (average (/ x y) y))
+   1))
+
+;;; way 2
+(define (sqrt x)
+  (fixed-point
+   (average-damp (lambda (y) (/ x y)))
+   1))
+
+(define average-damp
+  (lambda (f)
+    (lambda (x) (average (f x) x))))
+
+(define (fixed-point f start)
+  (define tolerance 0.00001)
+  (define (close-enuf? u v)
+    (< (abs (- u v)) tolerance))
+  (define (iter old new)
+    (if (close-enuf? old new)
+	new
+	(iter new (f new))))
+  (iter start (f start)))
